151397e6aba261d93c4fc311b95653799a7de2da
Kemal <kmlcanaydin@gmail.com>
2018-07-26 16:20:05 +0300

    validate update

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
     has_many :variants, dependent: :destroy
 
-    validates :name, presence: true
-    validates :price, presence: true, length: {minimum:3}
+    validates :name, presence: true, length: {minimum:3}
+    validates :price, presence: true, format: { with: /\A\d+\z/, message: "Integer only. No sign allowed." }
     
 end
