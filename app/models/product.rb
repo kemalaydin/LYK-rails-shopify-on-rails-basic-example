92dd104596252575055038bc80a2bfcd23a4f9f8
Kemal Aydın <kmlcanaydin@gmail.com>
2018-07-29 11:19:29 +0300

    validated update

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,7 +6,6 @@ class Product < ApplicationRecord
     accepts_nested_attributes_for :variants
 
     validates :name, presence: true, length: {minimum:3}
-    validates :price, presence: true, format: { with: /\A\d+\z/, message: "Integer only. No sign allowed." }
 
     ## Link yapısını 1-kemal-aydin yaptık, parametirize bizim için slug yapısı oluşturuyoru
     def to_param
