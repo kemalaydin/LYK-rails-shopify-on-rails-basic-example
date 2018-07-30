808cc695efe2e38c96f44b55d41f31d480ed466f
Kemal Aydın <kmlcanaydin@gmail.com>
2018-07-30 16:13:43 +0300

    tasklar yapıldı

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.destroy_all
+
+product_names = %w(shirt jean short sock swim sweater jacket skirt dress vest swimsuit)
+variant_names = %w(small medium large)
+
+product_names.each.with_index(1) do |product_name, i|
+  product = Product.create(name: product_name, price: i**4)
+  variant_names.each do |variant_name|
+    product.variants.create(name: variant_name, stock: 2*i)
+  end
+end
---
+++ lib/tasks/hello.rake
@@ -0,0 +1,10 @@
+namespace :hello do
+    desc "This task does nothing"
+    task :world do
+        puts "Hello world"
+    end
+
+    task :product do
+     
+    end
+end
