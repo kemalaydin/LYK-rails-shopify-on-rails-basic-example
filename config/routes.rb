bbc74cc2b8cb35bba475e2302cb43c56d321db8e
Kemal Aydın <kmlcanaydin@gmail.com>
2018-07-30 17:24:17 +0300

    rails ajax example added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root "products#index"
-  
+    
+    resources :books, only: [:index,:create,:destroy]
     ## multi lang desteği için 
     scope "/:locale" do
         resources :products do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_29_135627) do
+ActiveRecord::Schema.define(version: 2018_07_30_133636) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,14 @@ ActiveRecord::Schema.define(version: 2018_07_29_135627) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string "name"
+    t.string "isbn"
+    t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "products", force: :cascade do |t|
